ba53c2bd89f9b680908210c346bbe32981767b79
Jacob Shufro <jacob@shuf.ro>
2022-12-04 20:32:03 -0500

    Add json output/parsing to cli tool, refactor credentials.go a bit. Breaking change.

--- cli/main.go
+++ cli/main.go
@@ -5,8 +5,10 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 	"time"
@@ -17,13 +19,57 @@ import (
 
 const timestampFormat = "Mon 02 Jan 2006 3:04:05 PM MST"
 
+func parseToJson(credential string) (string, error) {
+
+	strs := strings.Split(credential, ":")
+	if len(strs) != 2 {
+		return "", fmt.Errorf("Invalid credential: %s\n", credential)
+	}
+
+	decoder := base64.NewDecoder(base64.URLEncoding, bytes.NewReader([]byte(strs[0])))
+	nodeID, err := io.ReadAll(decoder)
+	if err != nil {
+		return "", err
+	}
+
+	decoder = base64.NewDecoder(base64.URLEncoding, bytes.NewReader([]byte(strs[1])))
+	p, err := io.ReadAll(decoder)
+	if err != nil {
+		return "", err
+	}
+
+	ac := credentials.AuthenticatedCredential{}
+	err = proto.Unmarshal(p, ac.Pb())
+	if err != nil {
+		return "", err
+	}
+
+	ac.Credential.NodeId = nodeID
+
+	j, err := json.MarshalIndent(&ac, "", "    ")
+	return string(j), err
+}
+
 func main() {
 
 	nodeAddrFlag := flag.String("n", "", "Node address for which to generate the credential (required)")
 	timeFlag := flag.String("t", time.Now().Format(timestampFormat), "Timestamp to use")
 	secretFlag := flag.String("s", "test-secret", "Secret to use")
+	outputJsonFlag := flag.Bool("j", false, "Whether or not to print the credential in human-readable json")
+	parseToJsonFlag := flag.String("p", "", "Parses a credential and prints it in human-readable json")
 
 	flag.Parse()
+
+	if *parseToJsonFlag != "" {
+		str, err := parseToJson(*parseToJsonFlag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+			return
+		}
+		fmt.Println(str)
+		return
+	}
 
 	if *nodeAddrFlag == "" {
 		fmt.Println("Usage: cred-cli [OPTIONS]")
@@ -55,9 +101,21 @@ func main() {
 		return
 	}
 
+	if *outputJsonFlag {
+		j, err := json.MarshalIndent(cred, "", "    ")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+			return
+		}
+
+		fmt.Println(string(j))
+		return
+	}
+
 	cred.Credential.NodeId = nil
 
-	marshaled, err := proto.Marshal(cred)
+	marshaled, err := proto.Marshal(cred.Pb())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
--- credentials.go
+++ credentials.go
@@ -1,15 +1,68 @@
 package credentials
 
 import (
+	"bytes"
 	"crypto/hmac"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"hash"
+	"io"
+	"strings"
 	"time"
 
 	"github.com/Rocket-Pool-Rescue-Node/credentials/pb"
 	"github.com/pkg/errors"
 	"google.golang.org/protobuf/proto"
 )
+
+type AuthenticatedCredential pb.AuthenticatedCredential
+
+type jsonAuthenticatedCredential struct {
+	NodeID    string `json:"node_id"`
+	Timestamp int64  `json:"timestamp"`
+	Mac       string `json:"mac"`
+}
+
+func (ac *AuthenticatedCredential) Pb() *pb.AuthenticatedCredential {
+	return (*pb.AuthenticatedCredential)(ac)
+}
+
+func (ac *AuthenticatedCredential) MarshalJSON() ([]byte, error) {
+	var mac bytes.Buffer
+	nodeID := "0x" + hex.EncodeToString(ac.Credential.NodeId)
+	encoder := base64.NewEncoder(base64.URLEncoding, &mac)
+	encoder.Write(ac.Mac)
+	encoder.Close()
+
+	return json.Marshal(&jsonAuthenticatedCredential{
+		NodeID:    nodeID,
+		Timestamp: ac.Credential.Timestamp,
+		Mac:       string(mac.Bytes()),
+	})
+}
+
+func (ac *AuthenticatedCredential) UnmarshalJSON(data []byte) error {
+	var j jsonAuthenticatedCredential
+	ac.Pb().Reset()
+
+	if err := json.Unmarshal(data, &j); err != nil {
+		return err
+	}
+
+	ac.Credential = &pb.Credential{}
+	decoder := base64.NewDecoder(base64.URLEncoding, bytes.NewReader([]byte(j.Mac)))
+	decoded, err := io.ReadAll(decoder)
+	if err != nil {
+		return err
+	}
+
+	ac.Credential.NodeId = []byte(strings.TrimPrefix(j.NodeID, "0x"))
+	ac.Credential.Timestamp = j.Timestamp
+	ac.Mac = decoded
+	return nil
+}
 
 // CredentialManager authenticates and verifies rescue node credentials
 type CredentialManager struct {
@@ -23,7 +76,7 @@ func NewCredentialManager(h func() hash.Hash, key []byte) *CredentialManager {
 	}
 }
 
-func (c *CredentialManager) authenticateCredential(credential *pb.AuthenticatedCredential) error {
+func (c *CredentialManager) authenticateCredential(credential *AuthenticatedCredential) error {
 	// Serialize just the inner message so we can authenticate it and add it to the outer message
 	bytes, err := proto.Marshal(credential.Credential)
 	if err != nil {
@@ -38,11 +91,11 @@ func (c *CredentialManager) authenticateCredential(credential *pb.AuthenticatedC
 }
 
 // Create makes a new credential and authenticates it, returning a protoc struct that can be marshaled/unmarshaled
-func (c *CredentialManager) Create(timestamp time.Time, nodeID []byte) (*pb.AuthenticatedCredential, error) {
+func (c *CredentialManager) Create(timestamp time.Time, nodeID []byte) (*AuthenticatedCredential, error) {
 	if len(nodeID) != 20 {
 		return nil, fmt.Errorf("invalid nodeID length. Expected 20, got %d", len(nodeID))
 	}
-	message := pb.AuthenticatedCredential{}
+	message := AuthenticatedCredential{}
 	message.Credential = &pb.Credential{}
 	message.Credential.NodeId = nodeID
 	message.Credential.Timestamp = timestamp.Unix()
@@ -55,9 +108,9 @@ func (c *CredentialManager) Create(timestamp time.Time, nodeID []byte) (*pb.Auth
 }
 
 // Verify checks that a AuthenticatedCredential has a valid mac
-func (c *CredentialManager) Verify(authenticatedCredential *pb.AuthenticatedCredential) error {
+func (c *CredentialManager) Verify(authenticatedCredential *AuthenticatedCredential) error {
 	// Create a temporary AuthenticatedCredential and borrow the inner message from the provided credential
-	tmp := pb.AuthenticatedCredential{}
+	tmp := AuthenticatedCredential{}
 	tmp.Credential = authenticatedCredential.Credential
 
 	// Auth tmp
